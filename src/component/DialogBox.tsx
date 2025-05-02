cb4c99a3c8c5bcd4fe231e093a50f1231311e4f0
miracline2 <francesca4518@gmail.com>
2025-05-02 22:54:52 +0530

    Removed UnUsed Code

--- src/component/DialogBox.tsx
+++ src/component/DialogBox.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import Button from '@mui/material/Button';
 import toDOImage from '../assets/toDo.png'
 interface IDialogProps{
       open:boolean;
