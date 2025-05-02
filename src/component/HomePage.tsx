4b7b36c9e7d6f98fb869748a2d6dfb9f21b19011
miracline2 <francesca4518@gmail.com>
2025-05-02 14:20:17 +0530

    Implement Routing feature in Home page

--- src/component/HomePage.tsx
+++ src/component/HomePage.tsx
@@ -1,18 +1,25 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import toDO from '../assets/toDo.png';
+import { useNavigate } from 'react-router-dom';
 const HomePage = () => {
-
+const navigate = useNavigate();
   return (
-    <div className='flex justify-center items-center mx-auto'>
-        <Card sx={{ minWidth: 275, }}>
-            <p className='text-center text-black  font-bold text-2xl flex justify-center items-center mt-5'>Productivity</p>
+    <div className="h-full flex justify-center items-center flex-col">
+
+       <p className='text-center text-black  font-bold text-5xl flex justify-center items-center '>Productivity</p>
+        <Card sx={{ minWidth: 275,borderRadius: '16px' }}
+         className=" overflow-hidden border-4 border-black"
+         >
+           
             <img src={toDO} className='w-25 h-25' alt="ToDoImage" />
-        <CardActions className=' flex justify-center '>
-        <button className='border bg-orange-500 w-40 h-35 cursor-pointer rounded-lg hover:bg-orange-600'>Let's Started</button>
-      </CardActions>
+       
         </Card>
-      
+        <button 
+        className='border bg-orange-500 w-40 h-[50px] mt-5 cursor-pointer rounded-lg hover:bg-orange-600'
+        onClick={()=>{navigate('/toDO')}}
+        >
+          Let's Started
+        </button>
     </div>
   )
 }
